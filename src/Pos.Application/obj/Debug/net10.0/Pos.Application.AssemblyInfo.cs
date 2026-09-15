d4a0316e26ca6a1b33240376b0e8867ca873b927
DanielGB23 <i2310354@continental.edu.pe>
2026-09-14 20:00:02 -0500

    Nuevas funciones

--- src/Pos.Application/obj/Debug/net10.0/Pos.Application.AssemblyInfo.cs
+++ src/Pos.Application/obj/Debug/net10.0/Pos.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © NalunPos 2026")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61c988fb578278825c7f844ada9428ea55d0f88b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb2c7d3a2958acf8ad3168726ee10bee66275e45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pos.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
